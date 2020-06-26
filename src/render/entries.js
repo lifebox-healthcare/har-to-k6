43fab783a36ca8d4b3f91fa872e6b49d59e54273
Francis Devereux <francis@devrx.org>
2020-06-26 14:29:32 +0100

    Make previous 2 changes work for entries that aren't in a page group

--- src/render/entries.js
+++ src/render/entries.js
@@ -3,22 +3,8 @@ const entry = require('./entry')
 function entries(items) {
   const expanded = !!items.find((item) => item.state.expanded)
   const separator = expanded ? `\n\n` : `\n`
-  const want = /^https:\/\/.*(lifeboxhealth\.com|amazonaws\.com)\/.*$/
   if (items.length) {
-    let prevItem = undefined
-    const sections = items
-      .filter((item) => {
-        let address = item.request.address
-        // console.log(address)
-        // console.log(want.test(address))
-        // console.log(address[0])
-        return want.test(address)
-      })
-      .map((item) => {
-        let result = entry(item, prevItem)
-        prevItem = item
-        return result
-      })
+    const sections = items.map((item) => entry(item))
     return sections.filter((item) => item).join(separator)
   } else {
     return null
--- src/render/entry/index.js
+++ src/render/entry/index.js
@@ -1,10 +1,23 @@
 const header = require('./header')
 const logic = require('./logic')
 
-function entry(spec, prevSpec) {
-  return [header(spec, prevSpec), logic(spec, prevSpec)]
+const want = /^https:\/\/.*(lifeboxhealth\.com|amazonaws\.com)\/.*$/
+let prevSpec = undefined
+
+function entry(spec) {
+  let address = spec.request.address
+  // console.log(address)
+  // console.log(want.test(address))
+  if (!want.test(address)) {
+    return null
+  }
+
+  let result = [header(spec, prevSpec), logic(spec, prevSpec)]
     .filter((item) => item)
     .join(`\n`)
+
+  prevSpec = spec
+  return result
 }
 
 module.exports = entry
